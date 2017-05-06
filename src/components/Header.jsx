14819a646dd92016d3d671efec1949838cb54b18
Andrew Park <apark@movableink.com>
2017-05-06 13:50:09 -0400

    update header and add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+require('../styles/header.scss');
+
 export default class Header extends React.Component {
   render() {
     return (
       <div className="header">
         <img src={this.props.shop.shop_image_url} />
+        <div className="shop-info">
+          <h1>{this.props.shop.name}</h1>
+          <h3>{this.props.shop.location}</h3>
+        </div>
       </div>
     );
   }
