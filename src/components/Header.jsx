e872d8f85df4dbb43207ad5e79b3ba2cbd658133
Andrew Park <apark@movableink.com>
2017-05-06 19:48:45 -0400

    change header outline div to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ require('../styles/header.scss');
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="header">
+      <header className="header">
         <img src={this.props.shop.shop_image_url} />
         <div className="shop-info">
           <h1>{this.props.shop.name}</h1>
           <h3>{this.props.shop.location}</h3>
         </div>
-      </div>
+      </header>
     );
   }
 }
