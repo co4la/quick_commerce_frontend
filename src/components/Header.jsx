6cfb032dfe35fd3102fd6a7ab6575067167b795c
Andrew Park <apark@movableink.com>
2017-05-06 20:06:49 -0400

    update header class and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ require('../styles/header.scss');
 export default class Header extends React.Component {
   render() {
     return (
-      <header className="header">
+      <header>
         <img src={this.props.shop.shop_image_url} />
         <div className="shop-info">
           <h1>{this.props.shop.name}</h1>
