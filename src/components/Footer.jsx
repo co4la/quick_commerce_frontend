6070b48c36b411e8f1dc35c3a7ebd0322f4baacc
Andrew Park <apark@movableink.com>
2017-05-06 20:03:22 -0400

    add link icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,11 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        {this.props.shop.location}
+        <h4>{this.props.shop.location}</h4>
+        <div className="link-icons">
+          <i className="fa fa-facebook-square"></i>
+          <i className="fa fa-twitter-square"></i>
+        </div>
       </footer>
     )
   }
